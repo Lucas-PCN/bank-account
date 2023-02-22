a046af0ef44c969d18103f876560c167cedd7956
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-02-22 11:06:09 -0300

    Feat: implementa método main para interação com o usuário

--- src/main/java/com/betrybe/acc/java/bankaccount/Application.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/Application.java
@@ -1,5 +1,7 @@
 package com.betrybe.acc.java.bankaccount;
 
+import java.util.Scanner;
+
 /**
  * A class.
  */
@@ -9,7 +11,21 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    AccountNumberFormatter formatter = new AccountNumberFormatter();
 
+    System.out.println("Informe o número da conta:");
+
+    try {
+      int accountNumber = scanner.nextInt();
+      String account = formatter.formatAccountNumber(accountNumber);
+
+      System.out.println("Número da conta: " + accountNumber);
+    } catch (Exception e) {
+      System.out.println("Número da conta inválido! Apenas números são aceitos.");
+    }
+    
+    scan.close();
   }
 
 }
